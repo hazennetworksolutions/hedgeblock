c657a59fcf1f6b41ec9ede6d2261fa212f900059
hazennetworksolutions <161881320+hazennetworksolutions@users.noreply.github.com>
2024-11-24 20:25:39 +0300

    Create 0.1.0-autoscript.sh

---
+++ 0.1.0-autoscript.sh
@@ -0,0 +1,243 @@
+#!/bin/bash
+LOG_FILE="/var/log/hedgeblock_node_install.log"
+exec > >(tee -a "$LOG_FILE") 2>&1
+
+printGreen() {
+    echo -e "\033[32m$1\033[0m"
+}
+
+printLine() {
+    echo "------------------------------"
+}
+
+# Function to print the node logo
+function printNodeLogo {
+    echo -e "\033[32m"
+    echo "          
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+██████████████████████████████████████████████        ██████████████████████████████████████████████
+███████████████████████████████████████████              ███████████████████████████████████████████
+████████████████████████████████████████                    ████████████████████████████████████████
+█████████████████████████████████████                          █████████████████████████████████████
+█████████████████████████████████                                  █████████████████████████████████
+██████████████████████████████             █             █            ██████████████████████████████
+████████████████████████████           █████             ████           ████████████████████████████
+████████████████████████████          ██████             ██████         ████████████████████████████
+████████████████████████████          ██████             ██████          ███████████████████████████
+████████████████████████████          ███████            ██████          ███████████████████████████
+████████████████████████████          ██████████         ██████          ███████████████████████████
+████████████████████████████          █████████████      ██████          ███████████████████████████
+████████████████████████████             █████████████     ████          ███████████████████████████
+████████████████████████████          █     █████████████     █          ███████████████████████████
+████████████████████████████          █████     ████████████             ███████████████████████████
+████████████████████████████          ██████       ████████████          ███████████████████████████
+████████████████████████████          ██████          █████████          ███████████████████████████
+████████████████████████████          ██████             ██████          ███████████████████████████
+████████████████████████████          ██████             ██████          ███████████████████████████
+████████████████████████████          ██████             ██████         ████████████████████████████
+████████████████████████████            ████             ███            ████████████████████████████
+██████████████████████████████                                        ██████████████████████████████
+█████████████████████████████████                                  █████████████████████████████████
+█████████████████████████████████████                           ████████████████████████████████████
+████████████████████████████████████████                    ████████████████████████████████████████
+███████████████████████████████████████████              ███████████████████████████████████████████
+██████████████████████████████████████████████        ██████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+Hazen Network Solutions 2024 All rights reserved."
+    echo -e "\033[0m"
+}
+
+# Show the node logo
+printNodeLogo
+
+# User confirmation to proceed
+echo -n "Type 'yes' to start the installation Hedgeblock v0.1.0 and press Enter: "
+read user_input
+
+if [[ "$user_input" != "yes" ]]; then
+  echo "Installation cancelled."
+  exit 1
+fi
+
+# Function to print in green
+printGreen() {
+  echo -e "\033[32m$1\033[0m"
+}
+
+printGreen "Starting installation..."
+sleep 1
+
+printGreen "If there are any, clean up the previous installation files"
+
+sudo systemctl stop hedged
+sudo systemctl disable hedged
+sudo rm -rf /etc/systemd/system/hedged.service
+sudo rm $(which hedged)
+sudo rm -rf $HOME/.hedge
+sed -i "/hedged_/d" $HOME/.bash_profile
+
+# Update packages and install dependencies
+printGreen "1. Updating and installing dependencies..."
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install curl git wget htop tmux build-essential jq make lz4 gcc unzip -y
+
+# User inputs
+read -p "Enter your MONIKER: " MONIKER
+echo 'export MONIKER='$MONIKER
+read -p "Enter your PORT (2-digit): " PORT
+echo 'export PORT='$PORT
+
+# Setting environment variables
+echo "export MONIKER=$MONIKER" >> $HOME/.bash_profile
+echo "export HEDGE_CHAIN_ID=\"berberis-1\"" >> $HOME/.bash_profile
+echo "export HEDGE_PORT=$PORT" >> $HOME/.bash_profile
+source $HOME/.bash_profile
+
+printLine
+echo -e "Moniker:        \e[1m\e[32m$MONIKER\e[0m"
+echo -e "Chain ID:       \e[1m\e[32m$HEDGE_CHAIN_ID\e[0m"
+echo -e "Node custom port:  \e[1m\e[32m$HEDGE_PORT\e[0m"
+printLine
+sleep 1
+
+# Install Go
+printGreen "2. Installing Go..." && sleep 1
+cd $HOME
+VER="1.23.0"
+wget "https://golang.org/dl/go$VER.linux-amd64.tar.gz"
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf "go$VER.linux-amd64.tar.gz"
+rm "go$VER.linux-amd64.tar.gz"
+[ ! -f ~/.bash_profile ] && touch ~/.bash_profile
+echo "export PATH=\$PATH:/usr/local/go/bin:~/go/bin" >> ~/.bash_profile
+source $HOME/.bash_profile
+[ ! -d ~/go/bin ] && mkdir -p ~/go/bin
+
+# Version check
+echo $(go version) && sleep 1
+
+# Download Prysm protocol binary
+printGreen "3. Downloading Hedge binary and setting up..." && sleep 1
+wget -O hedged https://github.com/hedgeblock/testnets/releases/download/v0.1.0/hedged_linux_amd64_v0.1.0
+chmod +x hedged
+mkdir -p $HOME/.hedge/cosmovisor/genesis/bin
+mv $HOME/hedged $HOME/.hedge/cosmovisor/genesis/bin
+sudo ln -sf $HOME/.hedge/cosmovisor/genesis $HOME/.hedge/cosmovisor/current -f
+sudo ln -sf $HOME/.hedge/cosmovisor/current/bin/hedged /usr/local/bin/hedged -f
+
+# Create service file
+printGreen "6. Creating service file..." && sleep 1
+sudo tee /etc/systemd/system/hedged.service > /dev/null << EOF
+[Unit]
+Description=hedge node service
+After=network-online.target
+
+[Service]
+User=$USER
+ExecStart=$(which cosmovisor) run start
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=65535
+Environment="DAEMON_HOME=$HOME/.junction"
+Environment="DAEMON_NAME=junctiond"
+Environment="UNSAFE_SKIP_BACKUP=true"
+Environment="PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:$HOME/.hedge/cosmovisor/current/bin"
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+
+# Enable the service
+sudo systemctl daemon-reload
+sudo systemctl enable hedged
+
+# Initialize the node
+printGreen "7. Initializing the node..."
+hedged config set client chain-id ${HEDGE_CHAIN_ID}
+hedged config set client keyring-backend test
+hedged config set client node tcp://localhost:${HEDGE_PORT}657
+hedged init ${MONIKER} --chain-id ${HEDGE_CHAIN_ID}
+
+# Download genesis and addrbook files
+printGreen "8. Downloading genesis and addrbook..."
+curl -Ls https://raw.githubusercontent.com/hazennetworksolutions/hedgeblock/refs/heads/main/genesis.json > $HOME/.hedge/config/genesis.json
+wget -O $HOME/.hedge/config/addrbook.json "https://raw.githubusercontent.com/hazennetworksolutions/hedgeblock/refs/heads/main/addrbook.json"
+
+# Configure gas prices and ports
+printGreen "9. Configuring custom ports and gas prices..." && sleep 1
+sed -i.bak -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0.025uhedge\"/;" ~/.hedge/config/app.toml
+sed -i.bak -e "s%:1317%:${HEDGE_PORT}317%g;
+s%:8080%:${HEDGE_PORT}080%g;
+s%:9090%:${HEDGE_PORT}090%g;
+s%:9091%:${HEDGE_PORT}091%g;
+s%:8545%:${HEDGE_PORT}545%g;
+s%:8546%:${HEDGE_PORT}546%g;
+s%:6065%:${HEDGE_PORT}065%g" $HOME/.hedge/config/app.toml
+
+# Configure P2P and ports
+sed -i.bak -e "s%:26658%:${HEDGE_PORT}658%g;
+s%:26657%:${HEDGE_PORT}657%g;
+s%:6060%:${HEDGE_PORT}060%g;
+s%:26656%:${HEDGE_PORT}656%g;
+s%^external_address = \"\"%external_address = \"$(wget -qO- eth0.me):${HEDGE_PORT}656\"%;
+s%:26660%:${HEDGE_PORT}660%g" $HOME/.hedge/config/config.toml
+
+# Set up seeds and peers
+printGreen "10. Setting up peers and seeds..." && sleep 1
+seeds="7879005ab63c009743f4d8d220abd05b64cfee3d@54.92.167.150:26656"
+sed -i.bak -e "s/^seeds =.*/seeds = \"$seeds\"/" $HOME/.hedge/config/config.toml
+
+# Pruning Settings
+printGreen "12. Setting up pruning config..." && sleep 1
+sed -i -e "s/^pruning *=.*/pruning = \"custom\"/" $HOME/.hedge/config/app.toml 
+sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"100\"/" $HOME/.hedge/config/app.toml
+sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"19\"/" $HOME/.hedge/config/app.toml
+
+# Download the snapshot
+# printGreen "12. Downloading snapshot and starting node..." && sleep 1
+
+
+
+
+
+# Start the node
+printGreen "13. Starting the node..."
+sudo systemctl start hedged
+
+# Check node status
+printGreen "14. Checking node status..."
+sudo journalctl -u hedged -f -o cat
+
+# Verify if the node is running
+if systemctl is-active --quiet hedged; then
+  echo "The node is running successfully! Logs can be found at /var/log/hedgeblock_node_install.log"
+else
+  echo "The node failed to start. Logs can be found at /var/log/hedgeblock_node_install.log"
+fi
